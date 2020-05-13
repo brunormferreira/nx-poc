1b4ea28a36eba7d43b0a325add056d1c686c06fb
Bruno <brunormferreira@hotmail.com>
2020-05-13 11:24:28 -0300

    step8: create libraries

--- myorg/apps/todos/src/app/app.module.ts
+++ myorg/apps/todos/src/app/app.module.ts
@@ -3,10 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { UiModule } from '@myorg/ui';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule],
+  imports: [BrowserModule, HttpClientModule, UiModule],
   providers: [],
   bootstrap: [AppComponent]
 })
