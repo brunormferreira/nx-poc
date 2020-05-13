8836c8c69d3efd9c22044f30d11722440b11bc83
Bruno <brunormferreira@hotmail.com>
2020-05-13 10:50:33 -0300

    step3: display todos

--- myorg/apps/todos/src/app/app.component.ts
+++ myorg/apps/todos/src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 
+interface Todo {
+  title: string;
+}
+
 @Component({
   selector: 'myorg-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todos';
+  todos: Todo[] = [{ title: 'Todo 1' }, { title: 'Todo 2' }];
+
+  addTodo() {
+    this.todos.push({
+      title: `New todo ${Math.floor(Math.random() * 1000)}`
+    });
+  }
 }
