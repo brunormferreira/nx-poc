6515b4dd21f1a5fc21103c2340970a505a21a347
Bruno <brunormferreira@hotmail.com>
2020-05-13 11:03:30 -0300

    step5: add Node application

---
+++ myorg/apps/api/src/app/app.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@nestjs/common';
+
+interface Todo {
+  title: string;
+}
+
+@Injectable()
+export class AppService {
+  todos: Todo[] = [{ title: 'Todo 1' }, { title: 'Todo 2' }];
+
+  getData(): Todo[] {
+    return this.todos;
+  }
+
+  addTodo() {
+    this.todos.push({
+      title: `New todo ${Math.floor(Math.random() * 1000)}`
+    });
+  }
+}
